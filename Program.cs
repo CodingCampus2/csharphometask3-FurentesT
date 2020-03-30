e1860d8b612b5965607976997d1f6ea378ee533f
Taras Pobyvanets <furentes.t@gmail.com>
2020-03-30 15:11:16 +0300

    Homework has done

--- Program.cs
+++ Program.cs
@@ -16,15 +16,49 @@ namespace HomeworkTemplate
                 string UserLatitude = task.UserLatitude;
                 int placesAmount = task.DefibliratorStorages.Length;
 
+                float.TryParse(UserLatitude.Replace(',', '.'), out float fUserLatitude);
+                float.TryParse(UserLongitude.Replace(',','.'), out float fUserLongitude);
+
+                float distance = float.MaxValue;
+
+                const float degreeToRadCoef = 0.0174533f;
+
+                fUserLongitude *= degreeToRadCoef;
+                fUserLatitude *= degreeToRadCoef;
+
+                string resName = "", resAddress = ""; 
+
                 for (int i = 0; i < placesAmount; i++)
                 {
                     string defibliratorStorage = task.DefibliratorStorages[i];
-                }   
-                
-                return "My answer";
+
+                    string[] defibliratorStorageArr = defibliratorStorage.Split(';');
+                    
+                    float.TryParse(defibliratorStorageArr[2].Replace(',','.'), out float lognitude);
+                    float.TryParse(defibliratorStorageArr[3].Replace(',','.'), out float latitude);
+
+                    lognitude *= degreeToRadCoef;
+                    latitude *= degreeToRadCoef;
+
+                    float x = (lognitude - fUserLongitude) * MathF.Cos((latitude + fUserLatitude) / 2);
+                    float y = latitude - fUserLatitude;
+
+                    float d = MathF.Sqrt(x * x + y * y) * 6371f;
+
+                    if(d < distance)
+                    {
+                        distance = d;
+                        resName = defibliratorStorageArr[0];
+                        resAddress = defibliratorStorageArr[1];
+                    }
+                }
+                string result = string.Format("Name: {0}; Address: {1}", resName, resAddress);
+                System.Console.WriteLine(result);
+                return result;
             };
 
             Task3.CheckSolver(TaskSolver);
+
         }
     }
 }
